368452ad3c68076685b007d9f31dc7ed66e15ee4
hugoherculano <hugohercullano@gmail.com>
2023-02-11 02:09:46 -0300

    Settings OK

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send("Hello World");
-});
+import { categoriesRoutes } from "./routes/categories.routes";
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
