4dc6dc2818df78182c3515320b792b58bd020941
hugoherculano <hugohercullano@gmail.com>
2023-02-20 03:28:15 -0300

    documentation created to implement, completed file upload :rocket:

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,11 @@
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+import { ImportCategoryController } from "./ImportCategoryController";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+const importCategoryController = new ImportCategoryController(
+    importCategoryUseCase
+);
+
+export { importCategoryController };
